b75434337e1d36ddaea144ab0ca66ce2063cf819
Hugo Geissmann <hugo.geissmann@thiga.fr>
2017-08-08 12:04:42 +0200

    GTP -> GTM

--- footer.php
+++ footer.php
@@ -1,81 +1,49 @@
-		<div class="footer-container">
-		
-			
-		<footer class="details">
+<div class="footer-container">
+			<footer class="bg-primary short-2">
 				<div class="container">
 					<div class="row">
-						<div class="col-sm-3">
-							<img alt="logo" class="logo" src="/img/logo-thiga-black-min.png">
-							<p>
-								Thiga est une société pure player du Product Management. Pionnière de la discipline en France, la mission de Thiga est de faire entrer la création de produits numériques dans une nouvelle ère.
-							</p>
-						</div>
-						
-						<div class="col-sm-3">
-							<h1>Nos marques</h1>
-	
-								<a href="http://thiga.fr"> <img class="logo-foot" src="/img/productstrategy-min.png" ></a>
-								<a href="http://thiga.fr"><img class="logo-foot2" src="/img/productownership-min.png"><br></a>
-								<a href="http://growthagency.thiga.fr"><img class="logo-foot" 
-								src="/img/growth-min.png"></a>
-								<a href="http://academy.thiga.fr"><img class="logo-foot2" src="/img/academy-min.png"><br></a>
-						</div>
-						
-						<div class="col-sm-3">
-							<h1>Nos réseaux sociaux</h1>
-							<ul class="social-icons">
-								<li>
-									<a href="https://twitter.com/innothiga" target="_blank">
-										<i class="icon social_twitter"></i>
-									</a>
-								</li>
-								
-								<li>
-									<a href="https://www.facebook.com/thigaconsulting/" target="_blank">
-										<i class="icon social_facebook"></i>
-									</a>
-								</li>
-								
-								<li>
-									<a href="https://www.instagram.com/thigafr/" target="_blank">
-										<i class="icon social_instagram"></i>
-									</a>
-								</li>
-								
-								
-								
-								
-								
-								
-							</ul>
-						</div>
-						<div class="col-sm-3">
-							<h1>Nos labels</h1>
+						<div class="col-sm-12">
+							<span class="text-white sub">© Copyright 2017 THIGA
+							</span>
+							<span class="text-white">
+								<strong>Nos tribus</strong><br><br>
+							    <a href="http://productownership.thiga.fr">Thiga - Product Ownership</a><br>
+								<a href="http://productstrategy.thiga.fr">Thiga - Product Strategy</a><br>
+								<a href="http://growthagency.thiga.fr">Thiga - Growth Agency</a><br>
+								<a href="http://productdesign.thiga.fr">Thiga - Product Design</a>
+							</span>
+							<span class="text-white">
+								<strong>Centre de formation</strong><br><br>
+							    <a href="http://academy.thiga.fr">Thiga - Academy</a><br>
+							</span>
+							<span class="text-white">
+							<strong>Nos labels</strong><br><br>
 							<img class="logo-footh" src="/img/happyatwork.png" >
 						</div>
 					</div>
-					
-					<div class="row">
-						<div class="col-sm-12">
-							<span class="sub">© Copyright 2017 THIGA</span>
-						</div>
-					</div>
-					
 				</div>
-			</footer></div>
+				
+				<div class="contact-action">
+					<div class="align-vertical">
+						<i class="icon text-white icon_mail"></i>
+						<a href="contact.html" class="text-white"><span class="text-white">Nous contacter <i class="icon arrow_right"></i></span></a>
+					</div>
+				</div>
+			</footer>
+		</div>
 				
 		<script src="https://www.youtube.com/iframe_api"></script>
-		<script src="/js/jquery.min.js"></script>
-        <script src="/js/jquery.plugin.min.js"></script>
-        <script src="/js/bootstrap.min.js"></script>
-        <script src="/js/jquery.flexslider-min.js"></script>
-        <script src="/js/smooth-scroll.min.js"></script>
-        <script src="/js/skrollr.min.js"></script>
-        <script src="/js/spectragram.min.js"></script>
-        <script src="/js/scrollReveal.min.js"></script>
-        <script src="/js/isotope.min.js"></script>
-        <script src="/js/twitterFetcher_v10_min.js"></script>
-        <script src="/js/lightbox.min.js"></script>
-        <script src="/js/jquery.countdown.min.js"></script>
-        <script src="/js/scripts.js"></script>
+		<script src="js/jquery.min.js"></script>
+        <script src="js/jquery.plugin.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/skrollr.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/scrollReveal.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/twitterFetcher_v10_min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/scripts.js"></script>
     </body>
